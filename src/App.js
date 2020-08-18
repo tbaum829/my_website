4c7bf5fec4392e71cea75bab60fa48b91241d6c7
Tyler Baumgartner <tylerbaumgartner@Tylers-MacBook-Pro.local>
2020-08-18 01:46:12 -0400

    add some complexity

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Tyler Baumgartner</h1>
-      </header>
-    </div>
-  );
+class Name extends React.Component {
+  render() {
+    const full_name = this.props.first_name + " " + this.props.last_name;
+    return (
+      <h1>{ full_name }</h1>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first_name: "Tyler",
+      last_name: "Baumgartner"
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Name first_name={this.state.first_name} last_name={this.state.last_name} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
